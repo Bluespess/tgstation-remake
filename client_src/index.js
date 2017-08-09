b1df0fb6800df94f7e7562c33060a7f9c0a7ca66
monster860 <mnmaxim@gmail.com>
2017-08-08 22:39:00 -0400

    editor environment

--- client_src/index.js
+++ client_src/index.js
@@ -2,6 +2,12 @@
 const BluespessClient = require('bluespess-client');
 const $ = require('jquery');
 
-$(window).on("load", () => {
-	var client = new BluespessClient();
-});
+var client = new BluespessClient();
+
+if(global.is_bs_editor_env) {
+	module.exports = client;
+} else {
+	$(window).on("load", () => {
+		client.login();
+	});
+}
--- index.js
+++ index.js
@@ -3,15 +3,11 @@
 // BASED OFF OF tgstation COMMIT 6fbf68ccfcd28e7b99a1ffdfde24dc49576cb819
 
 const Bluespess = require('bluespess');
-const finalhandler = require('finalhandler')
-const http = require('http')
-const serveStatic = require('serve-static')
 
 console.log("Loading game..");
 
 var server = new Bluespess();
 global.server = server; // So the debugger can access it
-module.exports.server = server;
 
 server.importModule(require('./player.js'));
 server.importModule(require('./code/game/mobs/new_player.js'));
@@ -26,29 +22,34 @@ server.importModule(require('./code/modules/atmospherics/environmental/controlle
 server.importModule(require('./code/onclick/hud.js'));
 server.importModule(require('./code/onclick/inventory.js'));
 
-for(var x = -7; x <= 7; x++) for(var y = -7; y <= 7; y++) {
-	var turf = new Bluespess.Atom(server, {"components": ["SimulatedTurf"],"vars":{"appearance":{"icon":'icons/turf/floors.png',"icon_state":"floor"}}}, x, y, 0);
-	//turf.appearance.icon = 'icons/turf/floors.png';
-	//turf.appearance.icon_state = 'floor';
-}
-for(var i = 0; i < 5; i++) {
-	var template = {"components": ["Item"], "vars":{"appearance":{"icon":'icons/obj/items.png', "icon_state":"cuff_red","layer":2}}};
-	var a = new Bluespess.Atom(server, template, i, 0, 0);
-	console.log(template);
-	a.components.Item.inhand_icon_state = "coil_red";
-}
+if(global.is_bs_editor_env) {
+	module.exports = server;
+} else { // Load these packages within this if statement because they throw errors in atom.io
+	const finalhandler = require('finalhandler');
+	const http = require('http');
+	const serveStatic = require('serve-static');
+	
+	for(var x = -7; x <= 7; x++) for(var y = -7; y <= 7; y++) {
+		var turf = new Bluespess.Atom(server, {"components": ["SimulatedTurf"],"vars":{"appearance":{"icon":'icons/turf/floors.png',"icon_state":"floor"}}}, x, y, 0);
+		//turf.appearance.icon = 'icons/turf/floors.png';
+		//turf.appearance.icon_state = 'floor';
+	}
+	for(var i = 0; i < 5; i++) {
+		var template = {"components": ["Item"], "vars":{"appearance":{"icon":'icons/obj/items.png', "icon_state":"cuff_red","layer":2}}};
+		var a = new Bluespess.Atom(server, template, i, 0, 0);
+		console.log(template);
+		a.components.Item.inhand_icon_state = "coil_red";
+	}
 
-server.on("client_login", (client) => {
-	var template = {"components": ["Player", "MobInventory", "LivingMob"]};
-	var atom = new Bluespess.Atom(server, template, 0, 0, 0);
-	console.log(template);
-	atom.components.Mob.client = client;
-	atom.appearance.icon = 'icons/mob/human.png';
-	atom.appearance.icon_state = "skeleton";
-	atom.appearance.layer = 5;
-});
-
-if(require.main == module) {
+	server.on("client_login", (client) => {
+		var template = {"components": ["Player", "MobInventory", "LivingMob"]};
+		var atom = new Bluespess.Atom(server, template, 0, 0, 0);
+		console.log(template);
+		atom.components.Mob.client = client;
+		atom.appearance.icon = 'icons/mob/human.png';
+		atom.appearance.icon_state = "skeleton";
+		atom.appearance.layer = 5;
+	});
 	console.log("Starting server..");
 	var serve = serveStatic('./res/', {'index': ['index.html']});
 
