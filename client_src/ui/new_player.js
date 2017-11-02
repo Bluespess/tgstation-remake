e26dba2d48ba6fb7dc64d831fe4621d7c1c9853c
monster860 <mnmaxim@gmail.com>
2017-11-02 15:54:23 -0400

    button actions and css fucks

--- client_src/ui/new_player.js
+++ client_src/ui/new_player.js
@@ -7,13 +7,24 @@ class NewPlayerPanel {
 		this.build_content();
 	}
 
-	handle_message() {
-
+	handle_message(message) {
+		if(message.latejoin != undefined) {
+			this.panel.content_obj.getElementsByClassName("pregame")[0].style.display = message.latejoin ? "none" : "block";
+			this.panel.content_obj.getElementsByClassName("latejoin")[0].style.display = message.latejoin ? "visible" : "none";
+		}
 	}
 
 	build_content() {
+		this.panel.header_obj.classList.add("center");
+		this.panel.content_obj.classList.add("center");
 		this.panel.content_obj.innerHTML = `
-			<div><div class='button'>Setup Character</div></div>
+			<div class="vertical-margins"><div class='button' data-message='{"setup_character":true}'>Setup Character</div></div>
+			<div class="pregame vertical-margins">
+				<div class='button' data-radio-group='ready' data-radio-value='1'>Ready</div>
+				<div class='button selected' data-radio-group='ready' data-radio-value='0'>Not Ready</div>
+			</div>
+			<div class="latejoin vertical-margins"><div class='button' data-message='{"latejoin":true}'>Join Game</div></div>
+			<div class="vertical-margins"><div class='button' data-message='{"observe":true}'>Observe</div></div>
 		`;
 	}
 }
--- code/game/mobs/new_player_panel.js
+++ code/game/mobs/new_player_panel.js
@@ -3,7 +3,12 @@ const {Panel} = require('bluespess');
 
 class NewPlayerPanel extends Panel {
 	constructor(client) {
-		super(client, {width:250, height:265, title:"New Player Options", can_close: false});
+		super(client, {width:200, height:300, title:"New Player Options", can_close: false});
+	}
+
+	open() {
+		super.open();
+		this.send_message({"latejoin": false});
 	}
 }
 
