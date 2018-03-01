a23f4d8ea297291f5181c80d41c3cc14e574f97c
monster860 <mnmaxim@gmail.com>
2018-03-01 12:15:49 -0500

    loading maps log

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ if(global.is_bs_editor_env) {
 		//turf.icon = 'icons/turf/floors.png';
 		//turf.icon_state = 'floor';
 	}*/
+	console.log("Loading maps..");
 	server.instance_map(JSON.parse(fs.readFileSync('convert_test.bsmap', 'utf8')), 0, 0, 0);
 
 	server.on("client_login", (client) => {
