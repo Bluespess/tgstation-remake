44ed461963872f0d692c172c4e86b3893a36e51d
Unknown <epicmail709@yahoo.com>
2018-04-03 17:42:00 -0500

    Review

--- code/game/objects/items/weaponry.js
+++ code/game/objects/items/weaponry.js
@@ -17,10 +17,12 @@ BaseballBat.template = {
 				inhand_lhand_icon: 'icons/mob/inhands/weapons/melee_lefthand.dmi',
 				inhand_rhand_icon: 'icons/mob/inhands/weapons/melee_righthand.dmi',
 				inhand_icon_state: "baseball_bat",
-				size: 4,
+				size: 5,
 				force: 10,
-				throw_force: 12,
 				attack_verb: ["beat", "smacked"]
+			},
+			"Tangible": {
+				throw_force: 12
 			},
 			"Examine": {
 				desc: "There ain't a skull in the league that can withstand a swatter."
