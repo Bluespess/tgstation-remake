58c496e6888ae7c3e689730cbcf33acb86e7a46f
Unknown <epicmail709@yahoo.com>
2018-04-07 19:11:13 -0500

    Fixes

--- code/game/objects/buckling.js
+++ code/game/objects/buckling.js
@@ -32,7 +32,7 @@ class Buckle extends Component {
 	}
 
 	has_buckled_mobs() {
-		if(!this.buckled_mobs)
+		if(this.buckled_mobs == null)
 			return false;
 		if(this.buckled_mobs.length)
 			return true;
@@ -48,7 +48,7 @@ class Buckle extends Component {
 		if(check_loc && buckling.loc != this.a.loc) {
 			return false;
 		}
-		if((!this.can_buckle && !force) || buckling.buckled || (this.buckled_mobs.length >= this.max_buckled_mobs) || this.atom == buckling) { //TODO: restraints check
+		if((!this.can_buckle && !force) || buckling.c.LivingMob.buckled || (this.buckled_mobs.length >= this.max_buckled_mobs) || this.atom == buckling) { //TODO: restraints check
 			return false;
 		}
 
@@ -64,7 +64,7 @@ class Buckle extends Component {
 		this.buckled_mobs.push(buckling);
 		buckling.c.LivingMob.nomove_counter++;
 		buckling.c.MobHud.throw_alert('buckled', 'alert_buckled');
-		this.post_unbuckle_mob();
+		this.post_buckle_mob();
 		return true;
 	}
 
@@ -78,7 +78,7 @@ class Buckle extends Component {
 			unbuckling.c.MobHud.clear_alert('buckled');
 			let idx = this.buckled_mobs.indexOf(unbuckling);
 			if(idx != -1) this.buckled_mobs.splice(unbuckling, 1);
-			this.post_unbuckle_mob();
+			this.post_buckle_mob();
 		}
 	}
 
@@ -102,15 +102,16 @@ class Buckle extends Component {
 	user_buckle_mob(buckling, user, check_loc = true) { //This proc is unfinished placeholder, using it for testing.
 		//TODO: range/stat/restrained checks
 
-		this.buckle_mob(buckling, false, check_loc);
-		if(buckling == user) {
-			visible_message`<span class='notice'>${buckling} buckles themself to ${this.a}.</span>`
-				.self`<span class='notice'>You buckle yourself to ${this.a}.</span>`
-				.emit_from(this.a);
-		} else {
-			visible_message`<span class='warning'>${user} buckles ${buckling} to ${this.a}.</span>`
-				.self`<span class='warning'>${user} buckles you to ${this.a}.</span>`
-				.emit_from(this.a);
+		if(this.buckle_mob(buckling, false, check_loc)) {
+			if(buckling == user) {
+				visible_message`<span class='notice'>${buckling} buckles themself to ${this.a}.</span>`
+					.self`<span class='notice'>You buckle yourself to ${this.a}.</span>`
+					.emit_from(this.a);
+			} else {
+				visible_message`<span class='warning'>${user} buckles ${buckling} to ${this.a}.</span>`
+					.self`<span class='warning'>${user} buckles you to ${this.a}.</span>`
+					.emit_from(this.a);
+			}
 		}
 	}
 
@@ -127,8 +128,6 @@ class Buckle extends Component {
 		}
 	}
 }
-
-Buckle.depends = ["LivingMob", "MobHud"];
 
 Buckle.template = {
 	vars: {
