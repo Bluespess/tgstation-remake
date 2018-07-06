784e5151648f75aa6d9939c2da24fe82001c6617
ike709 <ike709@users.noreply.github.com>
2018-07-06 17:21:31 -0500

    Adds Floyd (#54)
    * Adds trash
    
    * Updates the maps

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ server.importModule(require('./code/game/objects/items/clothing.js'));
 server.importModule(require('./code/game/objects/items/emag.js'));
 server.importModule(require('./code/game/objects/items/storage.js'));
 server.importModule(require('./code/game/objects/items/tools.js'));
+server.importModule(require('./code/game/objects/items/trash.js'));
 server.importModule(require('./code/game/objects/items/weaponry.js'));
 server.importModule(require('./code/game/objects/structures/beds_chairs/chair.js'));
 server.importModule(require('./code/game/objects/structures/crates_lockers/base.js'));
