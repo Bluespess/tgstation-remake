b42e4a761d8032c21d2f43e713b28cad2c476d58
Chris Multhaupt <Stubenhocker1399@gmail.com>
2018-09-14 17:16:42 +0200

    Add deagle & variations (#65)
    * Add deagle & variations
    
    * Remove some deagle pasta

--- code/modules/projectiles/guns/ballistic/pistol.js
+++ code/modules/projectiles/guns/ballistic/pistol.js
@@ -24,5 +24,57 @@ module.exports.templates = {
 			icon_state: "pistol"
 		},
 		tree_paths: ["items/gun/ballistic/automatic/pistol"]
+	},
+	"deagle": {
+		components: ["BallisticGun", "BeltItem"],
+		vars: {
+			components: {
+				"BallisticGun": {
+					spawn_mag: "mag_m50",
+					mag_caliber: ".50",
+					mag_form_factor: "pistol",
+					tactical_reloads: true,
+					reload_sound: 'sound/weapons/autoguninsert.ogg',
+					can_suppress: false,
+					mag_state: true
+				},
+				"Examine": {
+					desc: "A robust .50 AE handgun."
+				},
+				"Item": {
+					size: 2
+				},
+			},
+			name: "desert eagle",
+			icon_state: "deagle"
+			
+		},
+		tree_paths: ["items/gun/ballistic/automatic/pistol/deagle"]
+	},
+	"deagleg": {
+		parent_template: "deagle",
+		vars: {
+			components: {
+				"Examine": {
+					desc: "A gold plated Desert Eagle folded over a million times by superior martian gunsmiths. Uses .50 AE ammo."
+				},
+			},
+			icon_state: "deagleg"
+			
+		},
+		tree_paths: ["items/gun/ballistic/automatic/pistol/deagle/gold"]
+	},
+	"deaglecamo": {
+		parent_template: "deagle",
+		vars: {
+			components: {
+				"Examine": {
+					desc: "A Deagle brand Deagle for operators operating operationally. Uses .50 AE ammo."
+				},
+			},
+			icon_state: "deaglecamo"
+			
+		},
+		tree_paths: ["items/gun/ballistic/automatic/pistol/deagle/camo"]
 	}
 };
